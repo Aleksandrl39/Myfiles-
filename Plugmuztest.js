96eb46b8044207e7c9304baac365fb3f368df165
Aleksandrl39 <40720446+Aleksandrl39@users.noreply.github.com>
2025-02-09 20:04:37 +0300

    Add files via upload

--- Plugmuztest.js
+++ Plugmuztest.js
@@ -1,28 +1,35 @@
-function addMusicButton() {
-    setTimeout(() => {
-        console.log("Добавляем кнопку 'Музыка' в меню...");
 
-        let menu = $('.menu__list').first();
-        let existingButton = menu.find('.menu__item:contains("Музыка")');
+(function() {
+    alert("Плагин Muzis загружен!");
+    console.log("Плагин Muzis загружен!");
 
-        if (existingButton.length === 0) {
-            let musicButton = $('<li class="menu__item selector focusable"><span>Музыка</span></li>');
+    function addMusicButton() {
+        setTimeout(() => {
+            console.log("Добавляем кнопку 'Музыка' в меню...");
 
-            musicButton.on('hover:enter', function() {
-                console.log("Кнопка 'Музыка' нажата, открываем поиск...");
-                Lampa.Activity.push({
-                    url: '',
-                    title: 'Поиск музыки',
-                    component: 'muzis_search'
+            let menu = $('.menu__list').first();
+            let existingButton = menu.find('.menu__item:contains("Музыка")');
+
+            if (existingButton.length === 0) {
+                let musicButton = $('<li class="menu__item selector focusable"><span>Музыка</span></li>');
+
+                musicButton.on('hover:enter', function() {
+                    console.log("Кнопка 'Музыка' нажата, открываем поиск...");
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'Поиск музыки',
+                        component: 'muzis_search'
+                    });
                 });
-            });
 
-            menu.append(musicButton);
-            console.log("Кнопка 'Музыка' добавлена!");
-        } else {
-            console.log("Кнопка 'Музыка' уже есть.");
-        }
-    }, 3000); // Ждём 3 секунды, чтобы убедиться, что меню загружено
-}
+                menu.append(musicButton);
+                console.log("Кнопка 'Музыка' добавлена!");
+                Lampa.Layer.update();
+            } else {
+                console.log("Кнопка 'Музыка' уже есть.");
+            }
+        }, 3000);
+    }
 
-addMusicButton();
+    addMusicButton();
+})();
