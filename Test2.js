4557bb61c5ef29f223de29afc195a64a98469dc5
Aleksandrl39 <40720446+Aleksandrl39@users.noreply.github.com>
2025-02-11 04:29:03 +0300

    Create Test2.js

---
+++ Test2.js
@@ -0,0 +1,30 @@
+function addMusicButton() {
+    setTimeout(() => {
+        console.log("Добавляем кнопку 'Музыка' в меню...");
+
+        let menu = $('.menu__list').first();
+        let existingButton = menu.find('.menu__item:contains("Музыка")');
+
+        if (existingButton.length === 0) {
+            let musicButton = $('<li class="menu__item selector focusable"><span>Музыка</span></li>');
+
+            musicButton.on('hover:enter', function() {
+                console.log("Кнопка 'Музыка' нажата, открываем страницу поиска...");
+                
+                Lampa.Activity.push({
+                    url: '',
+                    title: 'Поиск музыки',
+                    component: 'muzis_search',
+                    page: 1
+                });
+            });
+
+            menu.append(musicButton);
+            console.log("Кнопка 'Музыка' добавлена!");
+        } else {
+            console.log("Кнопка 'Музыка' уже есть.");
+        }
+    }, 3000);
+}
+
+addMusicButton();
