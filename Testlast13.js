614a9ec9b886d9744fb0bc95f26d2ab339db23b0
Aleksandrl39 <40720446+Aleksandrl39@users.noreply.github.com>
2025-02-12 07:29:41 +0300

    Create Testlast13.js

---
+++ Testlast13.js
@@ -0,0 +1,52 @@
+(function() {
+    var lastFmApiKey = "5e508b11704a1f7de4dd1ab7da50686d";
+
+    function searchMusic(query, callback) {
+        var url = `https://ws.audioscrobbler.com/2.0/?method=track.search&track=${encodeURIComponent(query)}&api_key=${lastFmApiKey}&format=json`;
+
+        $.ajax({
+            url: url,
+            method: 'GET',
+            success: function(response) {
+                var results = [];
+
+                if (response.results && response.results.trackmatches && response.results.trackmatches.track.length > 0) {
+                    response.results.trackmatches.track.forEach(function(track) {
+                        results.push({
+                            title: track.name + " - " + track.artist,
+                            poster: track.image[2]['#text'] || 'https://via.placeholder.com/200',
+                            link: `https://www.last.fm/music/${encodeURIComponent(track.artist)}/_/${encodeURIComponent(track.name)}`,
+                            type: 'music'
+                        });
+                    });
+                }
+
+                callback(results);
+            },
+            error: function() {
+                callback([]);
+            }
+        });
+    }
+
+    function addLastFmTab() {
+        Lampa.Search.addSource({
+            title: 'Музыка',
+            search: function(query, callback) {
+                searchMusic(query, function(results) {
+                    callback(results);
+                });
+            },
+            empty: function(callback) {
+                callback([]);
+            }
+        });
+    }
+
+    Lampa.Listener.follow('app', function(event) {
+        if (event.type === 'ready') {
+            addLastFmTab();
+        }
+    });
+
+})();
